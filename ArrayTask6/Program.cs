05422704cdbea2b24769d2816efca2f73e937c71
Corvette31 <corvette.ss@yandex.ru>
2022-06-27 13:34:44 +0300

    Добавьте файлы проекта.

---
+++ ArrayTask6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ArrayTask6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int arraySize = 30;
+            int itemMinValue = 0;
+            int itemMaxValue = 101;
+            int[] array = new int[arraySize];
+
+            Console.WriteLine("Исходный массив");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(itemMinValue, itemMaxValue);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine("\nОтсортированный массив\n");
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
